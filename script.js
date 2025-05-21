f98ae4c555b979b2ffa8495a9b8e7052671e34ca
Mohamad Khaeruman <103445614+Khaeruman1303@users.noreply.github.com>
2025-05-22 00:12:15 +0700

    ubah index, style, dan script

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ setInterval(nextSlide, 5000);
 
 // Initialize first slide
 showSlide(currentSlide);
+
+// Scroll navbar effect
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
